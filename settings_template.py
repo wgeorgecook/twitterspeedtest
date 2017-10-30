88975cf1df6e73088ea8455c28e596047b8dc519
William Cook <wgeorgecook@gmail.com>
2017-10-30 08:07:29 -0700

    Adding template settings file and updating README for public useability

---
+++ settings_template.py
@@ -0,0 +1,16 @@
+# Settings that can change
+# based on different circumstances
+
+# Insert your consumer and access keys here to be off the ground
+
+CONSUMER_SETTINGS = {
+    "consumer_key": "YOUR KEY",
+    "consumer_secret": "YOUR KEY",
+    }
+
+
+
+ACCESS_SETTINGS = {
+    "access_token": " YOUR KEY ",
+    "access_secret": " YOUR KEY ",
+    }
